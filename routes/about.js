28f08ad1ee49f1b88f8ff9fd0f35c217768a405d
vdtdg <valerian@dtdg.fr>
2022-06-12 20:45:22 +0200

    Change comments

--- routes/about.js
+++ routes/about.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const path = require('path');
 
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+/* GET about page. */
+router.get('/', function (req, res, next) {
     res.sendFile(path.join(__dirname, '../views/about.html'));
 });
 
--- routes/exoplanet.js
+++ routes/exoplanet.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 
-/* GET home page. */
+/* GET exoplanet page. */
 router.get('/', function (req, res, next) {
     res.sendFile(path.join(__dirname, '../views/exoplanet.html'));
 });
