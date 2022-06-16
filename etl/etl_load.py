49f7ab3a2addc9b6e7ebdefca4da4766e64345da
gdtdg <nimbmk05@hotmail.com>
2022-06-16 22:31:55 +0200

    ETL modifications with index mapping.

--- etl/etl_load.py
+++ etl/etl_load.py
@@ -7,6 +7,17 @@ from etl_logs import log_error
 es = Elasticsearch([es_url], basic_auth=auth, verify_certs=False)
 
 
+def delete_index(index_name):
+    es.options(ignore_status=[400, 404]).indices.delete(index=index_name)
+
+
+def create_index_with_mapping(index_name, body):
+    es.indices.create(
+        index=index_name,
+        body=body
+    )
+
+
 def load_in_elasticsearch(rows):
     actions = [transform_row_in_action(row) for row in rows]
     errors = bulk(client=es, actions=actions, stats_only=False, raise_on_error=False)
--- etl/etl_main.py
+++ etl/etl_main.py
@@ -4,13 +4,13 @@ import requests
 
 from config_file import csv_file_cleaned, csv_file
 from etl_extract import extract_csv, remove_hashtag_lines_in_csv
-from etl_load import load_in_elasticsearch, handle_response
+from etl_load import load_in_elasticsearch, handle_response, create_index_with_mapping, delete_index
 from etl_logs import log_summary, log_info
+from etl_mapping_elasticsearch import mapping
 from etl_transform import transform_row
 
 
 def main():
-
     remove_hashtag_lines_in_csv(csv_file, csv_file_cleaned)
     log_info("CSV file has been cleaned.", False)
 
@@ -20,8 +20,14 @@ def main():
     for row in reader:
         transform_row(row)
         transformed_rows.append(row)
-
     log_info("All rows transformed", False)
+
+    delete_index("exoplanet_csv")
+    log_info("Index deleted", False)
+
+    create_index_with_mapping("exoplanet_csv", mapping)
+    log_info("Index with mapping created", False)
+
     response = load_in_elasticsearch(transformed_rows)
     rows_posted, error_count = handle_response(response)
 
